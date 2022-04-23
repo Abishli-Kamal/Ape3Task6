8e902dba0e27b8ba018504d4f34cb3bfb36d041d
Abishli-Kamal <tu7ldxfzy@code.edu.az>
2022-04-24 02:44:28 +0400

    Task Ape3

---
+++ Ape3Task6/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+
+namespace Ape3Task6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student("Ziya Gachakhanli", "Assisant");
+            Console.WriteLine(student.GroupName);
+
+            Student student1 = new Student("Kamal Abishli", "Student");
+            Console.WriteLine(student1.GroupName);
+
+
+            Student student2 = new Student("Amal Gachakhanli", "Engineer");
+            Console.WriteLine(student2.GroupName);
+
+
+        }
+    }
+}
---
+++ Ape3Task6/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ape3Task6
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+        public static int count = 0;
+        public string Subject { get; set; }
+        public string Fullname { get; set; }
+        public int Number ;
+        public static int num=1000;
+        
+     
+        public string GroupName
+        {
+            get
+            {
+                
+                return Subject[0].ToString() + Subject[1].ToString().ToUpper() + Number.ToString();
+            }
+        }
+
+        public Student(string fullname, string subject)
+        {
+            num++;   
+            Number=num;
+            count++;
+            Id = count;
+            Fullname = fullname;
+            Subject = subject;
+
+        }
+    }
+}
